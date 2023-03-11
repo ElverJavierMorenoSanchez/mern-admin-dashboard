d7579d5a60fe048fe6e7948758ae0ad27721ae68
Javier Moreno <ejmoreno6@espe.edu.ec>
2023-03-11 13:49:20 -0500

    add managemente/admins endpoint

--- server/routes/management.routes.js
+++ server/routes/management.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { getAdmins } from "../controllers/management.controller.js";
 
 const router = Router();
+
+router.get("/admins", getAdmins);
 
 export default router;
